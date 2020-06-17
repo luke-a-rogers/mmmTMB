329de2f2e52143a72c47aaa541de54cf67d6a892
luke-a-rogers <luke.allister.rogers@gmail.com>
2020-06-17 14:42:11 -0700

    Added namespace cpp header

--- R/fit.R
+++ R/fit.R
@@ -1,4 +1,11 @@
-# Placeholder
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib mmmTMB, .registration = TRUE
+## usethis namespace: end
+NULL
+
+
+
 mmmTMB <- function() {
 
 }
