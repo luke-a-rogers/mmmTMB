53475afbeb406c2ac895a74b1aa391cb51afdd83
luke-a-rogers <luke.allister.rogers@gmail.com>
2020-11-06 20:17:17 -0800

    updated mmmTags() in progress

--- R/wrangle.R
+++ R/wrangle.R
@@ -6,7 +6,7 @@
 #' @param groups A list of named vectors. See details.
 #' @param time_step A character string. One of \code{year}, \code{month}, or
 #'   \code{day}.
-#' @param date_lims A character vector with two elements. "%Y-%M-%D".
+#' @param limits A character vector with two elements. "%Y-%M-%D".
 #' @param days_liberty Integer scalar. Minimum number of days at liberty.
 #'
 #' @details TBD
@@ -40,221 +40,234 @@
 #'   id = "ID")
 #' groups <- list(m = "M", f = "F")
 #' time_step <- "year"
-#' date_lims <- c("2010-01-01", "2020-12-31")
+#' limits <- c("2010-01-01", "2020-12-31")
 #' days_liberty <- 0L
 #'
-#' tags <- mmmTags(x, y, cols, groups, time_step, date_lims, days_liberty)
+#' tags <- mmmTags(x, y, cols, groups, time_step, limits, days_liberty)
 #'
 #'
 mmmTags <- function (x,
                      y,
                      cols = NULL,
                      groups = NULL,
-                     time_step = NULL,
-                     date_lims = NULL,
+                     step = NULL,
+                     limits = NULL,
                      days_liberty = NULL) {
 
-  # TODO: Check that group is identical between release and recover
+  # Check arguments ------------------------------------------------------------
 
-  #--------------- Check arguments --------------------------------------------#
-
+  # Arguments
   checkmate::assert_data_frame(x)
   checkmate::assert_data_frame(y)
   checkmate::assert_list(cols, unique = TRUE)
   checkmate::assert_list(groups, unique = TRUE, null.ok = TRUE)
-  checkmate::assert_character(time_step, len = 1, null.ok = TRUE)
+  checkmate::assert_character(step, len = 1, null.ok = TRUE)
+  checkmate::assert_choice(step, c("year", "month", "day"), null.ok = TRUE)
   checkmate::assert_integerish(days_liberty, lower = 0, len = 1, null.ok = TRUE)
+  # Data frame x column names
+  checkmate::assert_choice(cols$release_date, colnames(x))
+  checkmate::assert_choice(cols$release_area, colnames(x))
+  checkmate::assert_choice(cols$id, colnames(x))
+  # Data frame y column names
+  checkmate::assert_choice(cols$release_date, colnames(y))
+  checkmate::assert_choice(cols$release_area, colnames(y))
+  checkmate::assert_choice(cols$recover_date, colnames(y))
+  checkmate::assert_choice(cols$recover_area, colnames(y))
+  checkmate::assert_choice(cols$id, colnames(y))
 
-  #--------------- Check time step --------------------------------------------#
+  # Set defaults ---------------------------------------------------------------
 
-  if (is.null(time_step)) {
-    time_step <- "year"
-  } else if (is.element(time_step, c("year", "month", "day"))) {
-    time_step <- time_step
-  } else {
-    time_step <- "year"
+  # Groups
+  if (is.null(groups) | is.null(cols$group)) {
+    groups <- list(nogroups = 1L)
+    cols$group <- "nogroup"
+    x$nogroup <- 1L
+    y$nogroup <- 1L
+    cat("using no groups: groups or cols$group null")
   }
-
-  #--------------- Set default values -----------------------------------------#
-
+  # Time step
+  if (is.null(step)) {
+    step <- "year"
+    cat("using default time step: year")
+  }
+  # Days at liberty
   if (is.null(days_liberty)) {
-    if (time_step == "year") {
+    if (step == "year") {
       days_liberty <- 365L
-    } else if (time_step == "month") {
+    } else if (step == "month") {
       days_liberty <- 31L
     } else {
       days_liberty <- 1L
     }
   }
 
-  #--------------- Check required columns -------------------------------------#
+  # Check optional columns -----------------------------------------------------
 
-  checkmate::assert_true(is.element(cols$release_date, colnames(x)))
-  checkmate::assert_true(is.element(cols$release_area, colnames(x)))
-  checkmate::assert_true(is.element(cols$release_date, colnames(y)))
-  checkmate::assert_true(is.element(cols$release_area, colnames(y)))
-  checkmate::assert_true(is.element(cols$recover_date, colnames(y)))
-  checkmate::assert_true(is.element(cols$recover_area, colnames(y)))
-  checkmate::assert_true(is.element(cols$id, colnames(x)))
-  checkmate::assert_true(is.element(cols$id, colnames(y)))
+  checkmate::assert_choice(cols$group, colnames(x))
+  checkmate::assert_choice(cols$group, colnames(y))
 
-  #--------------- Check optional columns -------------------------------------#
-
-  if (is.null(cols$group)) {
-    cols$group <- "group"
-    x$group <- 1L
-    y$group <- 1L
-  }
-  checkmate::assert_true(is.element(cols$group, colnames(x)))
-  checkmate::assert_true(is.element(cols$group, colnames(y)))
-
-  #--------------- Rename columns ---------------------------------------------#
+  # Rename columns -------------------------------------------------------------
 
   # Rename columns x
   colnames(x)[which(colnames(x) == cols$release_date)] <- "release_date"
   colnames(x)[which(colnames(x) == cols$release_area)] <- "release_area"
-  colnames(x)[which(colnames(x) == cols$group)] <- "group"
+  colnames(x)[which(colnames(x) == cols$group)] <- "group_raw"
   colnames(x)[which(colnames(x) == cols$id)] <- "id"
   # Rename columns y
   colnames(y)[which(colnames(y) == cols$release_date)] <- "release_date"
   colnames(y)[which(colnames(y) == cols$release_area)] <- "release_area"
-  colnames(y)[which(colnames(y) == cols$group)] <- "group"
+  colnames(y)[which(colnames(y) == cols$group)] <- "group_raw"
   colnames(y)[which(colnames(y) == cols$recover_date)] <- "recover_date"
   colnames(y)[which(colnames(y) == cols$recover_area)] <- "recover_area"
   colnames(y)[which(colnames(y) == cols$id)] <- "id"
 
-  #--------------- Define x and y ---------------------------------------------#
+  # Define x and y -------------------------------------------------------------
 
+  # Define columns
   release_cols <- c("release_date", "release_area")
   recover_cols <- c("recover_date", "recover_area")
-  specify_cols <- c("group", "id")
+  specify_cols <- c("group_raw", "id")
   colnames_x <- c(release_cols, specify_cols)
   colnames_y <- c(release_cols, recover_cols, specify_cols)
-  # Define
+  # Define x and y
   x <- x[, colnames_x]
   y <- y[, colnames_y]
-
-  #--------------- Check column classes ---------------------------------------#
-
-  # TODO: Check column classes
-  # TODO: Check that groups classes and group column classes match
-
-
-  #--------------- Drop NA values ---------------------------------------------#
-
+  # Drop NA values
   x <- tidyr::drop_na(x)
   y <- tidyr::drop_na(y)
-
-  #--------------- Convert columns to dates -----------------------------------#
-
+  # Convert to dates
   x <- dplyr::mutate(x, release_date = lubridate::as_date(release_date))
-  y <- dplyr::mutate(y,
-    release_date = lubridate::as_date(release_date),
-    recover_date = lubridate::as_date(recover_date)
-  )
+  y <- dplyr::mutate(y, release_date = lubridate::as_date(release_date))
+  y <- dplyr::mutate(y, recover_date = lubridate::as_date(recover_date))
 
-  #--------------- Convert date limits to dates -------------------------------#
 
-  if (is.null(date_lims)) {
-    date_lims <- c(min(x$release_date), max(c(x$release_date, y$recover_date)))
+  # Check column class or type -------------------------------------------------
+
+  # Data frame x
+  checkmate::assert_date(x$release_date, any.missing = FALSE)
+  checkmate::assert_integerish(x$release_area, lower = 1, any.missing = FALSE)
+  checkmate::assert_integerish(x$group_raw, lower = 1, any.missing = FALSE)
+  checkmate::assert_character(x$id, len = 1, any.missing = FALSE)
+  # Data frame y
+  checkmate::assert_date(y$release_date, any.missing = FALSE)
+  checkmate::assert_date(y$recover_date, any.missing = FALSE)
+  checkmate::assert_integerish(y$release_area, lower = 1, any.missing = FALSE)
+  checkmate::assert_integerish(y$recover_area, lower = 1, any.missing = FALSE)
+  checkmate::assert_integerish(y$group_raw, lower = 1, any.missing = FALSE)
+  checkmate::assert_character(y$id, len = 1, any.missing = FALSE)
+
+  # Convert limits to dates ----------------------------------------------------
+
+  if (is.null(limits)) {
+    limits <- c(min(x$release_date), max(c(x$release_date, y$recover_date)))
   } else {
-    date_lims <- lubridate::as_date(date_lims)
+    limits <- lubridate::as_date(limits)
   }
 
-  #--------------- Filter events by dates -------------------------------------#
+  # Filter events by dates -----------------------------------------------------
 
   # Release
   x <- x %>% dplyr::filter(
-    release_date >= date_lims[1],
-    release_date <= date_lims[2])
+    release_date >= limits[1],
+    release_date <= limits[2])
   # Recover
   y <- y %>% dplyr::filter(
-    release_date >= date_lims[1],
-    release_date <= date_lims[2],
-    recover_date >= date_lims[1],
-    recover_date <= date_lims[2],
+    release_date >= limits[1],
+    release_date <= limits[2],
+    recover_date >= limits[1],
+    recover_date <= limits[2],
     release_date <= recover_date)
 
-  #--------------- Remove duplicates from x and y -----------------------------#
+  # Arrange columns ------------------------------------------------------------
 
-  # The tag reporting rate accounts for data losses due to errors
+  x <- x %>% dplyr::arrange(id, release_date, release_area, .keep_all = TRUE)
+  y <- y %>% dplyr::arrange(
+    id,
+    release_date,
+    release_area,
+    recover_date,
+    recover_area,
+    .keep_all = TRUE)
+
+  # Remove duplicate events ----------------------------------------------------
+
+  # Count the rows
   x_rows <- nrow(x)
   y_rows <- nrow(y)
+  # Remove duplicates
   x <- dplyr::distinct(x, id, release_date, release_area, .keep_all = TRUE)
   y <- dplyr::distinct(y, id, release_date, release_area, .keep_all = TRUE)
-  cat(paste0("duplicate release tags removed: ", x_rows - nrow(x)), "\n")
-  cat(paste0("duplicate recover tags removed: ", y_rows - nrow(y)), "\n")
+  # Report losses
+  cat("removed", x_rows - nrow(x), "duplicate release tags\n")
+  cat("removed", y_rows - nrow(y), "duplicate recover tags\n")
 
-  #--------------- Filter x by days at liberty --------------------------------#
+  # Filter x and y by days at liberty ------------------------------------------
 
-  # Insufficient days at liberty
   d <- dplyr::filter(y, recover_date - release_date < days_liberty)
-  # Remove d from x
   x <- dplyr::anti_join(x, d, by = colnames_x)[, colnames_x]
-
-  #--------------- Filter y by x ----------------------------------------------#
-
   y <- dplyr::inner_join(x, y, by = colnames_x)[, colnames_y]
 
-  #--------------- Index areas from zero --------------------------------------#
+  # Index areas from zero ------------------------------------------------------
 
   x <- dplyr::mutate(x, release_area = as.integer(release_area - 1L))
-  y <- dplyr::mutate(y, release_area = as.integer(release_area - 1L),
-                     recover_area = as.integer(recover_area - 1L))
+  y <- dplyr::mutate(y, release_area = as.integer(release_area - 1L))
+  y <- dplyr::mutate(y, recover_area = as.integer(recover_area - 1L))
 
-  #--------------- Convert group to integer -----------------------------------#
+  # Convert groups to integer --------------------------------------------------
 
   x <- dplyr::mutate(x, group_index = mmmGroup(group, groups))
   y <- dplyr::mutate(y, group_index = mmmGroup(group, groups))
 
+
+  # TODO: Continue from here
+  # TODO: Check that group is identical between release and recover
+
+
+
   #--------------- Convert date to time step ----------------------------------#
 
-  if (time_step == "year") {
+  if (step == "year") {
     x <- x %>% dplyr::mutate(
       release_step = as.integer(
-        lubridate::year(release_date) - lubridate::year(date_lims[1])))
+        lubridate::year(release_date) - lubridate::year(limits[1])))
     y <- y %>% dplyr::mutate(
       release_step = as.integer(
-        lubridate::year(release_date) - lubridate::year(date_lims[1])),
+        lubridate::year(release_date) - lubridate::year(limits[1])),
       recover_step = as.integer(
-        lubridate::year(recover_date) - lubridate::year(date_lims[1])))
+        lubridate::year(recover_date) - lubridate::year(limits[1])))
     # Convert liberty
     steps_liberty <- round(exp(log(days_liberty) - log(365.25)))
     cat(paste0("days at liberty: ", days_liberty,"; "))
     cat(paste0("steps (years) at liberty: ", steps_liberty[1]), "\n")
-  } else if (time_step == "month") {
+  } else if (step == "month") {
     x <- x %>% dplyr::mutate(
       release_step = as.integer(
-        12 * (lubridate::year(release_date) - lubridate::year(date_lims[1])) +
-          lubridate::month(release_date) - lubridate::month(date_lims[1])))
+        12 * (lubridate::year(release_date) - lubridate::year(limits[1])) +
+          lubridate::month(release_date) - lubridate::month(limits[1])))
     y <- y %>% dplyr::mutate(
       release_step = as.integer(
-        12 * (lubridate::year(release_date) - lubridate::year(date_lims[1])) +
-          lubridate::month(release_date) - lubridate::month(date_lims[1])),
+        12 * (lubridate::year(release_date) - lubridate::year(limits[1])) +
+          lubridate::month(release_date) - lubridate::month(limits[1])),
       recover_step = as.integer(
-        12 * (lubridate::year(recover_date) - lubridate::year(date_lims[1])) +
-          lubridate::month(recover_date) - lubridate::month(date_lims[1])))
+        12 * (lubridate::year(recover_date) - lubridate::year(limits[1])) +
+          lubridate::month(recover_date) - lubridate::month(limits[1])))
     # Convert liberty
     steps_liberty <- round(exp(log(days_liberty) - log(30)))
     cat(paste0("days at liberty: ", days_liberty[1], "; "))
     cat(paste0("steps (months) at liberty: ", steps_liberty[1]), "\n")
-  } else if (time_step == "day") {
+  } else if (step == "day") {
     x <- x %>% dplyr::mutate(
-      release_step = as.integer(release_date - date_lims[1]))
+      release_step = as.integer(release_date - limits[1]))
     y <- y %>% dplyr::mutate(
-      release_step = as.integer(release_date - date_lims[1]),
-      recover_step = as.integer(recover_date - date_lims[1]))
+      release_step = as.integer(release_date - limits[1]),
+      recover_step = as.integer(recover_date - limits[1]))
     steps_liberty <- days_liberty
     cat(paste0("days at liberty: ", days_liberty), "; ")
     cat(paste0("steps (days) at liberty: ", steps_liberty[1]), "\n")
   } else {
-    stop("time_step must be one of 'year', 'month', or 'day'.")
+    stop("step must be one of 'year', 'month', or 'day'.")
   }
 
-  #--------------- Drop NA values ---------------------------------------------#
-
-  x <- tidyr::drop_na(x)
-  y <- tidyr::drop_na(y)
 
   #--------------- Select columns ---------------------------------------------#
 
@@ -291,8 +304,8 @@ mmmTags <- function (x,
     mT = mT,
     mR = mR,
     groups = groups,
-    time_step = time_step,
-    date_lims = date_lims,
+    step = step,
+    limits = limits,
     days_liberty = days_liberty,
     steps_liberty = steps_liberty),
     class = "mmmTags"))
@@ -455,6 +468,82 @@ mmmWeights <- function (tags,
   #--------------- Return weights matrix --------------------------------------#
 
   return(mW)
+}
+
+
+#' Create Movement Index Matrix
+#'
+#' @param n [integer()] Number of areas.
+#' @param pattern [integer()] One of \code{0}: full movement, or \code{1}:
+#'   direct movement between numerically sequential neighbours only
+#' @param allow [integer()] [matrix()] Each row indicates directional
+#'   movement between a pair of areas indexed from \code{1}.
+#' @param disallow [integer()] [matrix()] As for \code{allow}, but
+#'   specified movement is disallowed.
+#'
+#' @return A square binary matrix
+#' @export
+#'
+#' @examples
+#'
+#' # Neighbours (default)
+#' mmmIndex(6)
+#'
+#' # Full movement
+#' mmmIndex(6, 0)
+#'
+#' # Neighbours plus 1-6, 2-5, and 5-2, but not 6-1
+#' allow <- matrix(c(1,6,2,5,5,2), ncol = 2, byrow = TRUE)
+#' mmmIndex(6, 1, allow)
+#'
+#' # Full minus 1-6
+#' disallow <- matrix(c(1,6), ncol = 2, byrow = TRUE)
+#' mmmIndex(6, 0, disallow = disallow)
+#'
+mmmIndex <- function (n, pattern = NULL, allow = NULL, disallow = NULL) {
+
+  # Check arguments
+  checkmate::assert_integerish(n, lower = 2, len = 1, any.missing = FALSE)
+  checkmate::assert_integerish(pattern, len = 1, null.ok = TRUE)
+  checkmate::assert_integerish(pattern, lower = 0, upper = 1, null.ok = TRUE)
+  checkmate::assert_integerish(pattern, any.missing = FALSE, null.ok = TRUE)
+  checkmate::assert_matrix(allow, mode = "integerish", null.ok = TRUE)
+  checkmate::assert_matrix(allow, any.missing = FALSE, null.ok = TRUE)
+  checkmate::assert_matrix(allow, ncols = 2, null.ok = TRUE)
+  # Initialize matrix
+  m <- matrix(0L, nrow = n, ncol = n)
+  # Default
+  if (is.null(pattern) & is.null(allow)) {
+    # Immediate neighbours only
+    for (i in seq_len(n - 1L)) {
+      m[i, i + 1L] <- 1L
+      m[i + 1L, i] <- 1L
+    }
+  }
+  # Add pattern
+  if (!is.null(pattern)) {
+    if (pattern) {
+      for (i in seq_len(n - 1L)) {
+        m[i, i + 1L] <- 1L
+        m[i + 1L, i] <- 1L
+      }
+    } else {
+      m <- matrix(1L, nrow = n, ncol = n)
+      diag(m) <- 0L
+    }
+  }
+  # Allow indexes
+  if (!is.null(allow)) {
+    m[allow] <- 1L
+    diag(m) <- 0L
+  }
+  # Disallow indexes
+  if (!is.null(disallow)) {
+    m[disallow] <- 0L
+    diag(m) <- 0L
+  }
+  # Return
+  return(m)
 }
 
 #' Map Values to Groups
