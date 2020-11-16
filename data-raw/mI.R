c84a147c51a51d23d236eddb34d3e4d6180f716b
luke-a-rogers <luke.allister.rogers@gmail.com>
2020-11-16 13:19:33 -0800

    removed old versions

--- data-raw/mI.R
+++
@@ -1,7 +0,0 @@
-# Create mI
-
-# Areas: 03
-mI_a03 <- create_movement_index(3)
-
-# Write to data/
-usethis::use_data(mI_a03, overwrite = TRUE)
--- data-raw/sim_template_2d.R
+++
@@ -1,6 +0,0 @@
-# Load
-load("~/github/mmmTMBarchive2/data/sim_template_2d_yrs_03a.rda")
-# Assign
-sim_template_2d <- sim_template_2d_yrs_03a
-# Write to data/
-usethis::use_data(sim_template_2d, overwrite = TRUE)
