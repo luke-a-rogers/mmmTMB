5a821cf62e7866ba94eecf91501a5cbfbf484136
luke-a-rogers <luke.allister.rogers@gmail.com>
2020-11-21 14:43:57 -0800

    updated with basic tests; removed old tests

--- tests/testthat/test-convert.R
+++ tests/testthat/test-convert.R
@@ -1,29 +1,25 @@
 context("test-convert.R")
 
-# test_that("create_movement_parameters_3d() matches movement_parameters_3d", {
-#   # Assign
-#   smp <- sysdata$sim_movement_parameters
-#   smp_3d <- sysdata$sim_movement_parameters_3d
-#   # Convert
-#   nv <- dim(smp_3d)[1]
-#   np <- dim(smp_3d)[2]
-#   ng <- dim(smp_3d)[3]
-#   mp_3d <- create_movement_parameters_3d(pars = smp, nv, np, ng)
-#   # Tests
-#   testthat::expect_equal(dim(mp_3d), dim(smp_3d))
-#   testthat::expect_equal(mp_3d, smp_3d)
-# })
-#
-# test_that("create_movement_probability_4d() matches movement_probability_4d", {
-#   # Assign
-#   smp_3d <- sysdata$sim_movement_parameters_3d
-#   smp_4d <- sysdata$sim_movement_probability_4d
-#   # Convert
-#   na <- dim(smp_4d)[1]
-#   nv <- dim(smp_4d)[3]
-#   ng <- dim(smp_4d)[4]
-#   mp_4d <- create_movement_probability_4d(na, nv, ng, smp_3d, sim_template_2d)
-#   # Tests
-#   testthat::expect_equal(dim(mp_4d), dim(smp_4d))
-#   testthat::expect_equal(mp_4d, smp_4d)
-# })
+test_that("logit() and invlogit() are inverses of eachother", {
+  # Inputs
+  x1 <- 0
+  x2 <- 0.5
+  x3 <- 1
+  y1 <- -Inf
+  y2 <- 0
+  y3 <- Inf
+  # Outputs
+  p1 <- invlogit(logit(x1))
+  p2 <- invlogit(logit(x2))
+  p3 <- invlogit(logit(x3))
+  r1 <- logit(invlogit(y1))
+  r2 <- logit(invlogit(y2))
+  r3 <- logit(invlogit(y3))
+  # Test
+  testthat::expect_equal(p1, x1)
+  testthat::expect_equal(p2, x2)
+  testthat::expect_equal(p3, x3)
+  testthat::expect_equal(r1, y1)
+  testthat::expect_equal(r2, y2)
+  testthat::expect_equal(r3, y3)
+})
