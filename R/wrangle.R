966575a7a0a6db1313548779f663a6f6608ddd06
luke-a-rogers <luke.allister.rogers@gmail.com>
2020-12-23 11:17:33 -0800

    now drops NAs from grouped data

--- R/wrangle.R
+++ R/wrangle.R
@@ -336,6 +336,7 @@ mmmTags <- function (x,
     dplyr::ungroup() %>%
     dplyr::distinct(.keep_all = TRUE) %>%
     dplyr::arrange(release_step, release_area, group) %>%
+    tidyr::drop_na() %>%
     as.matrix()
   # Confirm mode
   if (typeof(x) != "integer") {
@@ -361,6 +362,7 @@ mmmTags <- function (x,
       group,
       recover_step,
       recover_area) %>%
+    tidyr::drop_na() %>%
     as.matrix()
   # Confirm mode
   if (typeof(y) != "integer") {
